8fc8591110502fa20a712075eb22ab6c20115667
Shantanu Shubham <shantanu0707@gmail.com>
2023-05-12 12:05:58 +0530

    Add 3.3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,41 @@
 const { createNewNote, addToNote, readNote } = require("./notes");
-const { verifyIfEmail } = require("./myValidator");
+const yargs = require("yargs");
 
-createNewNote("Let's start learning with Tutorac!");
+// const action = process.argv[2];
 
-addToNote("Hi! I am learning with Tutorac!");
+// if (action === "read") {
+//   readNote();
+// } else if (action === "write") {
+//   createNewNote("This is new Note");
+// } else if (action === "add") {
+//   addToNote("Note was added.");
+// }
 
-readNote();
+yargs.version("2.1.0");
 
-console.log(verifyIfEmail("www.tutorac.com"));
+yargs.command({
+  command: "add",
+  describe: "This is used to add a new Note.",
+  handler: function () {
+    // console.log("This command will add a new note.");
+    addToNote("Note was added.");
+  },
+});
 
-console.log("This is after installing Nodemon");
+yargs.command({
+  command: "read",
+  describe: "This is used to read all the notes.",
+  handler: function () {
+    readNote();
+  },
+});
+
+yargs.command({
+  command: "write",
+  describe: "This command will create a new note and delete the old one.",
+  handler: function () {
+    createNewNote("This is new Note");
+  },
+});
+
+console.log(yargs.argv);
